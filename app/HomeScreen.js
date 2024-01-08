3206f9de2046b1263d78fc9c40e11097344274f7
Rana-Mapscrew <ranapaul741223@gmail.com>
2024-01-08 23:40:40 +0530

    init

--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/native-stack'
 
-export default function App() {
+export default function Home() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        
-      </Stack.Navigator>
-
-
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
